02dac0715c50fefddc83440aa4d4becbc9fb1848
Admin <admin@Admins-MacBook-Pro.local>
2018-11-28 09:54:40 +0700

    fixed icon

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="page-container md-layout-column">
     <md-toolbar class="md-primary">
-      <md-button class="md-icon-button" @click="showNavigation = true">
-        <md-icon>menu</md-icon>
-      </md-button>
+      <svg class="icon" viewBox="0 0 24 24" @click='showNavigation = true'>
+        <path fill="#fff" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+      </svg>
       <span class="md-title">{{messages.ep}} 1</span>
 
       <div class="md-toolbar-section-end">
         <md-button class="md-icon-button" @click='toDevelopers'>
-          <md-icon>group</md-icon>
+          <svg class="icon" viewBox="0 0 24 24">
+            <path fill="#fff" d="M16,13C15.71,13 15.38,13 15.03,13.05C16.19,13.89 17,15 17,16.5V19H23V16.5C23,14.17 18.33,13 16,13M8,13C5.67,13 1,14.17 1,16.5V19H15V16.5C15,14.17 10.33,13 8,13M8,11A3,3 0 0,0 11,8A3,3 0 0,0 8,5A3,3 0 0,0 5,8A3,3 0 0,0 8,11M16,11A3,3 0 0,0 19,8A3,3 0 0,0 16,5A3,3 0 0,0 13,8A3,3 0 0,0 16,11Z" />
+          </svg>
         </md-button>
       </div>
     </md-toolbar>
@@ -136,6 +138,10 @@ export default {
 .lang-active {
   cursor: pointer;
   color: #000;
+}
+.icon{
+    width: 24px;
+    height: 24px;
 }
 .lang-inactive {
   cursor: pointer;
